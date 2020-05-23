0fbe02b5e768222e7f6bb5e841f9e8e9c233f97d
Leonardo Araujo Silva <leonardo.araujo@matera.com>
2020-05-22 23:49:08 -0300

    Creating logic of the game

---
+++ game/src/main/java/studying/neural/wanderer/domain/Direction.java
@@ -0,0 +1,21 @@
+package studying.neural.wanderer.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Direction {
+    NORTH(0, -1),
+    SOUTH(0, 1),
+    EAST(1, 0),
+    WEST(-1, 0);
+
+    private final int movementX;
+    private final int movementY;
+
+    private Direction(int movementX, int movementY) {
+        this.movementX = movementX;
+        this.movementY = movementY;
+    }
+
+
+}
